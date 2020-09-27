70d43fd4ed0ec0a7ea41450ccfbed30ab9b5d43b
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-27 13:21:45 -0500

    Add createAuction handler

--- src/handlers/hello.js
+++
@@ -1,8 +0,0 @@
-async function hello(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello from hello.handler' }),
-  };
-}
-
-export const handler = hello;
