4ac5a67bcd40fe4b6cfddc1b7cfa455a0be1449f
Irvin Reyes <reyes.yerp@gmail.com>
2020-09-27 23:19:47 -0500

    Update createAuction handler

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,7 +1,16 @@
 async function createAuction(event, context) {
+  const { title } = JSON.parse(event.body);
+  const now = new Date();
+
+  const auction = {
+    title,
+    status: 'OPEN',
+    createdAt: now.toISOString(),
+  };
+
   return {
-    statusCode: 200,
-    body: JSON.stringify({ event, context }),
+    statusCode: 201,
+    body: JSON.stringify(auction),
   };
 }
 
