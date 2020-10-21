b850c1625e8967fd2754e3f36bb02b4a27c0bc32
Irvin Reyes <reyes.yerp@gmail.com>
2020-10-21 10:20:54 -0500

    Create a common middleware

--- src/handlers/createAuction.js
+++ src/handlers/createAuction.js
@@ -1,10 +1,8 @@
-import middy from '@middy/core';
-import httpJsonBodyParser from '@middy/http-json-body-parser';
-import httpEventNormalizer from '@middy/http-event-normalizer';
-import httpErrorHandler from '@middy/http-error-handler';
 import AWS from 'aws-sdk';
 import createError from 'http-errors';
 import { v4 as uuid } from 'uuid';
+
+import commonMiddleware from '../lib/commonMiddleware';
 
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
@@ -36,7 +34,4 @@ async function createAuction(event, context) {
   };
 }
 
-export const handler = middy(createAuction)
-  .use(httpJsonBodyParser())
-  .use(httpEventNormalizer())
-  .use(httpErrorHandler());
+export const handler = commonMiddleware(createAuction);
