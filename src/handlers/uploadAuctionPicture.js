ea24f737eddd88c57f3f117997898cc9ae9bbb1b
Irvin Reyes <reyes.yerp@gmail.com>
2020-10-26 13:41:11 -0600

    Add validation for auction pictures

--- src/handlers/uploadAuctionPicture.js
+++ src/handlers/uploadAuctionPicture.js
@@ -1,14 +1,25 @@
 import middy from '@middy/core';
 import httpErrorHandler from '@middy/http-error-handler';
+import validator from '@middy/validator';
 import createError from 'http-errors';
 
 import { getAuctionById } from './getAuction';
 import { uploadPictureToS3 } from '../lib/uploadPictureToS3';
 import { setAuctionPictureUrl } from '../lib/setAuctionPictureUrl';
+import uploadAuctionPictureSchema from '../lib/schemas/uploadAuctionPictureSchema';
 
 async function uploadAuctionPicture(event, context) {
   const { id } = event.pathParameters;
+  const { email } = event.requestContext.authorizer;
   const auction = await getAuctionById(id);
+
+  // Validate auction ownership
+  if (auction.seller !== email) {
+    throw new createError.Forbidden(
+      `You are not allowed to upload a picture for this auction!`,
+    );
+  }
+
   const base64 = event.body.replace(/^data:image\/\w+;base64,/, '');
   const buffer = Buffer.from(base64, 'base64');
 
@@ -28,4 +39,6 @@ async function uploadAuctionPicture(event, context) {
   };
 }
 
-export const handler = middy(uploadAuctionPicture).use(httpErrorHandler());
+export const handler = middy(uploadAuctionPicture)
+  .use(httpErrorHandler())
+  .use(validator({ inputSchema: uploadAuctionPictureSchema }));
